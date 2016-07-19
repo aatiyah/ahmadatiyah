82adaab59893b6bea43f8022cb5580d674afe140
aatiyah <medatiyah@gmail.com>
2016-07-19 14:41:01 -0500

    Added friendly_id and configured for articles and projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -64,11 +64,11 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      @project = Project.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :description, :github, :web, :image)
+      params.require(:project).permit(:title, :description, :github, :web, :image, :slug)
     end
 end
